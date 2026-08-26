5556448e6c3bef44ebf9d3669ec73cfa20f747a5
Trace Macias <trmacias@calpoly.edu>
2026-08-26 10:37:28 -0700

    start managing states now

--- vite-project/src/MyApp.jsx
+++ vite-project/src/MyApp.jsx
@@ -2,12 +2,31 @@
 import React from "react";
 import Table from "./Table";
 
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor"
+  },
+  {
+    name: "Mac",
+    job: "Bouncer"
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress"
+  },
+  {
+    name: "Dennis",
+    job: "Bartender"
+  }
+];
+
 function MyApp() {
   return (
-    <div className="container">
-      <Table />
-    </div>
-  );
+  <div className="container">
+    <Table characterData={characters} />
+  </div>
+);
 }
 
 export default MyApp;
--- vite-project/src/Table.jsx
+++ vite-project/src/Table.jsx
@@ -1,35 +1,38 @@
 // src/Table.jsx
-import React from "react";
-
-function Table() {
+function TableHeader() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
   );
 }
 
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    );
+   }
+  );
+  return (
+      <tbody>
+        {rows}
+       </tbody>
+   );
+}
+
+function Table(props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} />
+      </table>
+    );
+}
 export default Table;

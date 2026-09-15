1f4e2cee6faf11c25b6981d56845168cc8cfa3d1
Trace Macias <trmacias@calpoly.edu>
2026-09-15 13:01:45 -0700

    Add TDD reflection to Jest tests

--- jest/sample-functions.test.js
+++ jest/sample-functions.test.js
@@ -35,3 +35,8 @@ test("Testing containsNumbers -- mixed text with spaces", () => {
   const result = myFunctions.containsNumbers("abc def");
   expect(target).toBe(result);
 });
+
+//Following the TDD model was challenging. I'm used to writing code first and then test
+// ing it. I had to think about the tests first and then write the code to make the tests pass.
+// It took me more time than I thought it would, but this approach is beneificial when
+// designing a feature that can deal with weird inputs. 
